2b1d6209147caaf0f8e4c311c188d4a43d11a30d
Anadil <mdanadil32@gmail.com>
2026-06-24 09:55:54 +0530

    Duplicate contain

---
+++ Arrays/ContainDuplicate.java
@@ -0,0 +1,18 @@
+public class ContainDuplicate {
+
+    public static boolean FindDuplicateVlaue(int numbers[]){
+        for(int i =0; i < numbers.length; i++){
+            for(int j= i+1; j< numbers.length; j++){
+                if(numbers[i] == numbers[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int numbers [] = {1,2,3,1};
+        System.out.print(FindDuplicateVlaue(numbers));
+
+    }
+}
