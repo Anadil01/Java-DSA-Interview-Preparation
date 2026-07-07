6abd562a5b6b78028272e2092d4eafc0e498f9ed
Anadil <mdanadil32@gmail.com>
2026-07-07 10:20:29 +0530

    Largest String in Array

---
+++ String/largestStr.java
@@ -0,0 +1,16 @@
+public class largestStr{
+    public static void main(String[] args) {
+        String fruits[] = {"apple" , "mongo" , "banana"};
+
+        String largest = fruits[0];
+
+        for(int i =0 ; i < fruits.length; i++){
+            if(largest.compareTo(fruits[i]) < 0){
+                largest = fruits[i];
+            } 
+        }
+        System.out.print(largest);
+
+
+    }
+}
